eb56a045476a81baada1bcc736d514663f1c571e
Alex-B012 <rad15on2014@gmail.com>
2026-02-27 19:02:14 +0300

    admin panel - first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+// import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "../../frontend/src/components/ScrollToTop.jsx";
+
+const App = () => {
+  // const location = useLocation();
+
+  return (
+    <div className="App mx-4 md:mx-[10%] sm:mx-[5%]">
+      {/* <ScrollToTop /> */}
+      footer App
+    </div>
+  );
+};
+
+export default App;
